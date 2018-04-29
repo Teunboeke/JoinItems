061014560ad139e296682f814e8048465783bf06
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-29 19:28:24 +0100

    Add files via upload

---
+++ src/JoinItems/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JoinItems\Main;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+    
+    public function onEnable(): void {
+        $this->getLogger->info("Plugin has been enabled.");
+    }
+    public function onJoin(PlayerJoinEvent $event){
+        $inv = $player->getInventory();
+        $inv->clearAll();
+        
+        $hub = Item::get(345, 1, 1);
+        $hub->setCustomName(TextFormat::GOLD . "Server Selector!");
+        $inv->setItem(0, $hub);
+    }
+}
